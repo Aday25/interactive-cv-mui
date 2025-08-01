8ed5c0100297407f398f3f46e32d0302b5903a82
Aday25 <aday.alvarez84@gmail.com>
2025-08-01 19:16:33 +0200

    Setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/interactive-cv-mui/',  // 👈 nombre de tu repo
   plugins: [react()],
 })
